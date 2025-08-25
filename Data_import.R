f66ea546ebf490a653731c6aee64d4eaacc4bebe
m-taubert <72388188+m-taubert@users.noreply.github.com>
2025-08-25 15:55:04 +0200

    Add files via upload

--- Data_import.R
+++ Data_import.R
@@ -3,11 +3,11 @@
 # if anything goes wrong in the following scripts, try rerunning this here first
 
 # import ASV table
-asv <- readRDS("asv.RData")
+asv <- readRDS("import/asv.RData")
 nrow(asv)
 
 # import metadata
-meta <- read.csv(file="metadata.csv", header=TRUE, sep=";")
+meta <- read.csv(file="import/metadata.csv", header=TRUE, sep=";")
 meta <- meta[,-1]
 rownames(meta) <- meta$sample
 meta <- meta %>% dplyr::rename(qPCR=X16S.gene.copies.per.L, qPCR.dev=dev)
@@ -17,6 +17,6 @@ summary(meta)
 identical(meta$sample,rownames(asv))
 
 # import taxonomy data
-taxa.raw <- readRDS("taxa.RData")
+taxa.raw <- readRDS("import/taxa.RData")
 taxa <- cbind(sequence=rownames(taxa.raw), data.frame(taxa.raw, row.names=NULL)) # put sequences in own row
 taxa[1:10,2:7]
